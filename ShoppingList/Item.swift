0a98c75636a588c6dec0082996d660cf131f793b
Dennis Nedkov <Dnedkov9274@jh-mba-jacks.local>
2019-02-12 11:28:19 -0600

    added Item class

---
+++ ShoppingList/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  ShoppingList
+//
+//  Created by Dennis Jivko Nedkov on 2/12/19.
+//  Copyright © 2019 John Hersey High School. All rights reserved.
+//
+
+import Foundation
+class Item {
+    
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+    
+}
